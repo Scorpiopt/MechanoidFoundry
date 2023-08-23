8891b7bd26109f643cee7310f0458f6338a17c1f
Taranchuk <nfhfyxer@gmail.com>
2023-08-23 21:16:58 +0300

    update

--- 1.4/Source/MechanoidFoundry/MechanoidFoundry/HarmonyPatches/JobGiver_Recharge_TryGiveJob_Patch.cs
+++ 1.4/Source/MechanoidFoundry/MechanoidFoundry/HarmonyPatches/JobGiver_Recharge_TryGiveJob_Patch.cs
@@ -6,31 +6,6 @@ using VFEMech;
 
 namespace MechanoidFoundry
 {
-    [HarmonyPatch(typeof(JobGiver_ReturnToStationIdle), "TryGiveJob")]
-    public static class JobGiver_ReturnToStationIdle_TryGiveJob_Patch
-    {
-        public static bool Prefix(Pawn pawn)
-        {
-            var building = CompMachine.cachedMachinesPawns[pawn].myBuilding;
-            if (building == null)
-            {
-                var comp = pawn.TryGetComp<CompMachine>();
-                if (comp != null && comp.myBuilding is null)
-                {
-                    building = Helpers.GetAvailableMechanoidPad(pawn, pawn);
-                    if (building != null)
-                    {
-                        CompMachine.cachedMachinesPawns[pawn].myBuilding = building;
-                    }
-                    else
-                    {
-                        return false;
-                    }
-                }
-            }
-            return true;
-        }
-    }
     [HarmonyPatch(typeof(JobGiver_Recharge), "TryGiveJob")]
     public static class JobGiver_Recharge_TryGiveJob_Patch
     {
